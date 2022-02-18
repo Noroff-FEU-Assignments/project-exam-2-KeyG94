0d79556cb7342aed6b55a16cd53acd5e89790e56
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-02-18 22:56:41 +0100

    added background to html, added logo

--- holidaze/components/layout/Layout.js
+++ holidaze/components/layout/Layout.js
@@ -1,15 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/Asset 6.svg";
 import styles from "./Layout.module.scss";
 
 export default function Layout({ children }) {
   return (
     <div className="container">
-      <nav className="flex pt-4 pb-4 justify-between">
+      <nav className="flex pt-6 pb-6 justify-between">
         {/* Company log  */}
-        <div className="w-1/3">HOLIDAZE</div>
+        <div className="w-1/3">
+          <div className="w-32">
+            <Image src={logo} layout="responsive" alt="holidaze logo" />
+          </div>
+        </div>
 
-        <div className="text-center w-1/3">
-          <div className="flex justify-around bg-white w-32 ">
+        <div className="w-1/3">
+          <div className="flex justify-around bg-white w-32 pt-2 ">
             <Link href="/">Home</Link>
             <Link href="/results">Results</Link>
           </div>
@@ -17,7 +23,7 @@ export default function Layout({ children }) {
 
         {/* display hidden on small devices, this is in footer  */}
         <div className={styles.linksContainer}>
-          <div className="flex justify-around w-36">
+          <div className="flex justify-around w-36 pt-2">
             <Link href="/contact">Contact</Link>
             <Link href="/login">Login</Link>
           </div>
