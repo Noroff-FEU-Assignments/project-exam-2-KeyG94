74bd028096e2fbddd7ff716be203ac315a5432df
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-02-19 20:12:31 +0100

    added background details, and created a component from the search icon

--- holidaze/components/layout/Layout.js
+++ holidaze/components/layout/Layout.js
@@ -1,10 +1,22 @@
 import Nav from "./Nav";
+
 export default function Layout({ children }) {
   return (
-    <div className="container">
-      <Nav />
-      <div className="body-container">{children}</div>
-      <footer className="">Footer</footer>
-    </div>
+    <>
+      <div className="container relative">
+        <Nav />
+        <div className="body-container">{children}</div>
+      </div>
+      <footer className=" backdrop-blur-sm backdrop-brightness-[.25] text-[#A1A1A1] p-8">
+        <div className="container flex justify-between flex-wrap ">
+          <span>© 2022 Glenn Key. All Rights Reserved.</span>
+          <span>
+            <a href="/contact" className="hover:text-white">
+              Contact
+            </a>
+          </span>
+        </div>
+      </footer>
+    </>
   );
 }
--- holidaze/pages/index.js
+++ holidaze/pages/index.js
@@ -1,6 +1,8 @@
 import Layout from "../components/layout/Layout";
 import Head from "../components/layout/Head";
 import { useState } from "react";
+import SearchIcon from "../components/SearchIcon";
+import SearchbarDropDown from "../components/SearchbarDropDown";
 
 export default function Home() {
   const [search, setSearch] = useState("");
@@ -8,35 +10,23 @@ export default function Home() {
   const handleChange = ({ target }) => {
     setSearch(target.value);
   };
-  console.log(search);
 
   return (
     <Layout>
       <Head title="home" />
       <div className="grid place-items-center h-screen">
         <div className="flex flex-col text-center text-white">
-          <h1 className="text-5xl font-bold ">Find your reservation today!</h1>
-          <div className="flex py-4 px-2 my-4 bg-lightBlack mx-auto w-2/3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8 mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
+          <h1 className="text-5xl font-bold">Find your reservation today!</h1>
+          <div className="flex relative py-4 px-2 my-4 bg-lightBlack mx-auto w-2/3">
+            <SearchIcon />
             <input
               className="outline-none text-[20px] bg-lightBlack"
               placeholder="Search"
               onChange={(e) => handleChange(e)}
               value={search}
             />
+            {/* add prop from data from api  */}
+            {!search ? "" : <SearchbarDropDown />}
           </div>
           <p>Some Long descriptive text or catch phrace</p>
         </div>
