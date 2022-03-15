9f470975019ddeaf1940e97ec343adf2725a57f4
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-03-15 20:18:25 +0100

    added cloudinary as a config domain

--- holidaze/next.config.js
+++ holidaze/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	images: {
-		domains: ["upload.wikimedia.org", "enigmatic-sierra-33843.herokuapp.com"],
-	},
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "enigmatic-sierra-33843.herokuapp.com",
+      "res.cloudinary.com",
+    ],
+  },
 };
 
 module.exports = nextConfig;
