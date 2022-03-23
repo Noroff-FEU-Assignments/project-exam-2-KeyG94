9ed09a446d4d7036f12020145cb14dc722a0c5a3
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-03-23 22:59:06 +0100

    made structural changes, moved the auth checker to admin and try to redirect to login instead of the opposite

--- holidaze/components/global/admin/DashboardAdm.jsx
+++ holidaze/components/global/admin/DashboardAdm.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import AdminNav from "../../layout/AdminNav";
 import Head from "../../layout/Head";
 import Dashboard from "../../continents/Dashboard.jsx";
+import AuthContext from "../../../context/AuthContext";
+import Router from "next/router";
 
 export default function DashboardAdm() {
-	return (
-		<>
-			<Head title='Dashboard' />
-			<div className='sm:flex'>
-				<AdminNav current='dashboard' />
-				<Dashboard dashboard='admin' />
-			</div>
-		</>
-	);
+  //get state of the authentication provider
+  const [auth, setAuth] = useContext(AuthContext);
+
+  if (auth) {
+    return (
+      <>
+        <Head title="Dashboard" />
+        <div className="sm:flex">
+          <AdminNav current="dashboard" />
+          <Dashboard dashboard="admin" />
+        </div>
+      </>
+    );
+  }
+
+  if (!auth) {
+    Router.push("login");
+  }
 }
--- holidaze/components/global/home/HomePage.jsx
+++ holidaze/components/global/home/HomePage.jsx
@@ -6,40 +6,42 @@ import styles from "./Home.module.scss";
 import { useState } from "react";
 
 export default function HomePage({ api }) {
-	const [search, setSearch] = useState("");
-	const [data, setData] = useState(api);
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState(api);
 
-	return (
-		<div className='flex justify-center items-center min-h-screen'>
-			<div className='flex flex-col text-center text-white mx-auto'>
-				<div className=' w-3/4 mx-auto'>
-					<h1 className='text-3xl sm:text-5xl font-bold'>Find your reservation today!</h1>
-					<div className='w-3/4 mx-auto'>
-						<div className='flex relative py-4 px-2 my-4 bg-darkBlack'>
-							<SearchIcon />
-							<input
-								className='outline-none text-[20px] w-full bg-darkBlack'
-								placeholder='Search'
-								onChange={({ target }) => setSearch(target.value)}
-								value={search}
-							/>
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col text-center text-white mx-auto">
+        <div className=" w-3/4 mx-auto">
+          <h1 className="text-3xl sm:text-5xl font-bold">
+            Find your reservation today!
+          </h1>
+          <div className="w-3/4 mx-auto">
+            <div className="flex relative py-4 px-2 my-4 bg-darkBlack">
+              <SearchIcon />
+              <input
+                className="outline-none text-[20px] w-full bg-darkBlack"
+                placeholder="Search"
+                onChange={({ target }) => setSearch(target.value)}
+                value={search}
+              />
 
-							{!search ? "" : <SearchbarDropDown data={data} search={search} />}
-						</div>
-						<p className={styles.descriptiveP}>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-							incididunt ut labore et dolore magna aliqua.
-						</p>
-						{/* add 2 buttons here */}
-						<div className={styles.linksContainer}>
-							<div className='flex-wrap flex justify-around mt-8'>
-								<Link href='/contact'>Contact</Link>
-								<Link href='/login'>Login</Link>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+              {!search ? "" : <SearchbarDropDown data={data} search={search} />}
+            </div>
+            <p className={styles.descriptiveP}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            {/* add 2 buttons here */}
+            <div className={styles.linksContainer}>
+              <div className="flex-wrap flex justify-around mt-8">
+                <Link href="/contact">Contact</Link>
+                <Link href="/admin">Login</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
