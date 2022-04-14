19e77d9aab4c6940e4a24a943878c6c8d577300e
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-04-14 16:18:37 +0200

    Changed link in the layout to use next link

--- holidaze/components/layout/Layout.js
+++ holidaze/components/layout/Layout.js
@@ -1,20 +1,21 @@
+import Link from "next/link";
 import Nav from "./Nav";
 
 export default function Layout({ children }) {
-	return (
-		<>
-			<Nav />
-			<main>{children}</main>
-			<footer className='backdrop-blur-sm backdrop-brightness-[.25] text-[#A1A1A1] p-8'>
-				<div className='container flex flex-col sm:flex sm:flex-row sm:justify-between '>
-					<span>© 2022 Glenn Key. All Rights Reserved.</span>
-					<span>
-						<a href='/contact' className='hover:text-white'>
-							Contact
-						</a>
-					</span>
-				</div>
-			</footer>
-		</>
-	);
+  return (
+    <>
+      <Nav />
+      <main>{children}</main>
+      <footer className="backdrop-blur-sm backdrop-brightness-[.25] text-[#A1A1A1] p-8">
+        <div className="container flex flex-col sm:flex sm:flex-row sm:justify-between ">
+          <span>© 2022 Glenn Key. All Rights Reserved.</span>
+          <span>
+            <Link href="/contact" className="hover:text-white">
+              Contact
+            </Link>
+          </span>
+        </div>
+      </footer>
+    </>
+  );
 }
