f76d14810050b3e8b439c82d2cb5be0d0baacf0a
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-04-14 16:20:05 +0200

    Removed uneccesary console log

--- holidaze/components/countries/SearchbarDropDown.jsx
+++ holidaze/components/countries/SearchbarDropDown.jsx
@@ -2,55 +2,54 @@ import { useState } from "react";
 import BookNow from "./BookNow.jsx";
 
 export default function SearchbarDropDown({ data, search }) {
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [modalProduct, setModalProduct] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalProduct, setModalProduct] = useState({});
 
-	console.log(modalProduct);
-
-	return (
-		<ul className='absolute w-full -ml-2 mt-12 text-left bg-darkBlack '>
-			{isModalOpen && (
-				<BookNow
-					product={modalProduct}
-					closeModal={() => {
-						setIsModalOpen(false);
-						setModalProduct(null);
-					}}
-				/>
-			)}
-			{
-				/* loop through results  */
-				data.map(({ attributes, id }, index) => {
-					const { hotel_name, hotel_description, hotel_location, hotel_image } = attributes;
-					if (
-						hotel_name.toLowerCase().includes(search.toLowerCase()) ||
-						hotel_location.toLowerCase().includes(search.toLowerCase())
-					) {
-						return (
-							<div key={index}>
-								<div className='flex justify-between p-3 border-b-2 hover:bg-lightBlack hover:cursor-pointer'>
-									<span>{hotel_name}</span>
-									<span>{hotel_location}</span>
-									<button
-										onClick={() => {
-											setIsModalOpen(true);
-											setModalProduct({
-												id,
-												hotel_name,
-												hotel_location,
-												hotel_image,
-												hotel_description,
-											});
-										}}
-									>
-										Book now
-									</button>
-								</div>
-							</div>
-						);
-					}
-				})
-			}
-		</ul>
-	);
+  return (
+    <ul className="absolute w-full -ml-2 mt-12 text-left bg-darkBlack ">
+      {isModalOpen && (
+        <BookNow
+          product={modalProduct}
+          closeModal={() => {
+            setIsModalOpen(false);
+            setModalProduct(null);
+          }}
+        />
+      )}
+      {
+        /* loop through results  */
+        data.map(({ attributes, id }, index) => {
+          const { hotel_name, hotel_description, hotel_location, hotel_image } =
+            attributes;
+          if (
+            hotel_name.toLowerCase().includes(search.toLowerCase()) ||
+            hotel_location.toLowerCase().includes(search.toLowerCase())
+          ) {
+            return (
+              <div key={index}>
+                <div className="flex justify-between p-3 border-b-2 hover:bg-lightBlack hover:cursor-pointer">
+                  <span>{hotel_name}</span>
+                  <span>{hotel_location}</span>
+                  <button
+                    onClick={() => {
+                      setIsModalOpen(true);
+                      setModalProduct({
+                        id,
+                        hotel_name,
+                        hotel_location,
+                        hotel_image,
+                        hotel_description,
+                      });
+                    }}
+                  >
+                    Book now
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        })
+      }
+    </ul>
+  );
 }
