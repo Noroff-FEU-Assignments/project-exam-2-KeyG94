69dea826fb403f879e218f80dda5e1adafc28fef
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-04-14 22:10:32 +0200

    Added green to tailwind colors

--- holidaze/tailwind.config.js
+++ holidaze/tailwind.config.js
@@ -1,39 +1,43 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		container: {
-			center: true,
-		},
-		// color scheme
-		colors: {
-			orange: "#F19A3E",
-			lightBlack: "#313132",
-			darkBlack: "#161618",
-			silver: "#eaeaea",
-			white: "#FFF",
-			black: "#000",
-			grey: "#ACACAC",
-			red: "#EF626C",
-		},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    // color scheme
+    colors: {
+      orange: "#F19A3E",
+      lightBlack: "#313132",
+      darkBlack: "#161618",
+      silver: "#eaeaea",
+      white: "#FFF",
+      black: "#000",
+      grey: "#ACACAC",
+      red: "#EF626C",
+      green: "#09B43A",
+    },
 
-		fontSize: {
-			xs: ".512rem",
-			sm: ".64rem",
-			tiny: ".8rem",
-			base: "1rem",
-			lg: "1.25rem",
-			xl: "1.563rem",
-			"2xl": "1.953rem",
-			"3xl": "2.441rem",
-			"4xl": "3.052rem",
-		},
-		screens: {
-			xs: "530px",
-			...defaultTheme.screens,
-		},
-		extend: {},
-	},
-	plugins: [],
+    fontSize: {
+      xs: ".512rem",
+      sm: ".64rem",
+      tiny: ".8rem",
+      base: "1rem",
+      lg: "1.25rem",
+      xl: "1.563rem",
+      "2xl": "1.953rem",
+      "3xl": "2.441rem",
+      "4xl": "3.052rem",
+    },
+    screens: {
+      xs: "530px",
+      ...defaultTheme.screens,
+    },
+    extend: {},
+  },
+  plugins: [],
 };
