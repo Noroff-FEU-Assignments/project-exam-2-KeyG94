cb641ad94e31e1b9a1f0a7164b0f3e4cc9132016
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-04-16 21:16:36 +0200

    Added new custom loader when user clicks the product

--- holidaze/components/countries/ResultCard.jsx
+++ holidaze/components/countries/ResultCard.jsx
@@ -1,30 +1,38 @@
+import { useState } from "react";
 import Image from "next/image";
 import placeholder from "../../public/image-blur-placeholder.png";
 import Link from "next/link";
+import GKLoadingModal from "../../components/global/utills/GKLoadingModal";
 
-export default function ResultCard({ name, id, location, image }) {
-	return (
-		<div key='id' className='group relative'>
-			<div className='mt-4 flex justify-between text-white'>
-				<p>{name ? name : "Loading.."}</p>
-				<p>{id ? id : "Loading.."}</p>
-			</div>
-			<div className='bg-gray-200 aspect-square rounded-md lg:h-80 lg:aspect-none cursor-pointer'>
-				<Link href={`/${id}`} passHref>
-					<Image
-						src={image ? image : placeholder}
-						placeholder='blur'
-						blurDataURL={placeholder}
-						alt={`Image of ${name}`}
-						className='object-center hover:scale-105 transition'
-						width={1}
-						height={1}
-						layout='responsive'
-						objectFit='cover'
-					/>
-				</Link>
-			</div>
-			<p className='text-white'>{location ? location : "Loading.."}</p>
-		</div>
-	);
-}
+const ResultCard = ({ name, id, location, image }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <div key="id" className="group relative">
+      {isLoading && <GKLoadingModal />}
+      <div className="mt-4 flex justify-between text-white">
+        <p>{name ? name : "Loading.."}</p>
+        <p>{id ? id : "Loading.."}</p>
+      </div>
+      <Link href={`/${id}`} passHref>
+        <div
+          className="bg-gray-200 aspect-square rounded-md lg:h-80 lg:aspect-none cursor-pointer"
+          onClick={() => setIsLoading(true)}
+        >
+          <Image
+            src={image ? image : placeholder}
+            alt={`Image of ${name}`}
+            className="object-center hover:scale-105 transition"
+            width={1}
+            height={1}
+            layout="responsive"
+            objectFit="cover"
+          />
+        </div>
+      </Link>
+      <p className="text-white">{location ? location : "Loading.."}</p>
+    </div>
+  );
+};
+
+export default ResultCard;
