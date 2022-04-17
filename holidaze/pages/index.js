d7f1fb591d38ec321e6460781e3d31d07fecec9b
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-04-17 12:39:04 +0200

    added a revalidate prop to index to fetch new data every 400 seconds

--- holidaze/pages/index.js
+++ holidaze/pages/index.js
@@ -5,37 +5,40 @@ import axios from "axios";
 import { BASE_URL, HOTELS } from "../constants/baseUrl";
 
 export default function Home({ hotel }) {
-	return (
-		<Layout>
-			<Head title='home' />
-			<HomePage api={hotel} />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Head title="home" />
+      <HomePage api={hotel} />
+    </Layout>
+  );
 }
 
 export async function getStaticProps() {
-	// in case there is an error in the API call
-	// we'll send an empty array in as the prop
-	const url = BASE_URL + HOTELS;
-	let hotel = [];
+  // in case there is an error in the API call
+  // we'll send an empty array in as the prop
+  const url = BASE_URL + HOTELS;
+  let hotel = [];
 
-	try {
-		const response = await axios.get(url);
+  try {
+    const response = await axios.get(url);
 
-		// the log here will happen on the server, you can check the console in your editor
-		if (response.statusText !== "OK") {
-			console.log(`Status: ${response.status} \n Status text: ${response.statusText}`);
-		}
-		// the array is on the response.data.data property
-		hotel = response.data.data;
-	} catch (error) {
-		console.log(error);
-	}
+    // the log here will happen on the server, you can check the console in your editor
+    if (response.statusText !== "OK") {
+      console.log(
+        `Status: ${response.status} \n Status text: ${response.statusText}`
+      );
+    }
+    // the array is on the response.data.data property
+    hotel = response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
 
-	// the props object we return here will become the props in the component
-	return {
-		props: {
-			hotel,
-		},
-	};
+  // the props object we return here will become the props in the component
+  return {
+    props: {
+      hotel,
+    },
+    revalidate: 400,
+  };
 }
