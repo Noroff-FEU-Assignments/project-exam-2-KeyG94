b8446ba8b16ecd77ccf5c50fa223b0a4ef3e12b1
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2022-04-30 08:14:42 +0200

    Moved the loading bar slightly lower

--- holidaze/components/global/utills/GKLoadingModal.jsx
+++ holidaze/components/global/utills/GKLoadingModal.jsx
@@ -4,7 +4,7 @@ import * as ImIcon from "react-icons/im";
 const GKLoadingModal = () => {
   return (
     <GKModalBackground>
-      <div className="bg-white rounded-md w-[100px] grid justify-center mx-auto my-[33%] p-3 bg-opacity-90 shadow">
+      <div className="bg-white rounded-md w-[100px] grid justify-center mx-auto my-[40%] p-3 bg-opacity-90 shadow">
         <ImIcon.ImSpinner2 className="text-4xl text-orange animate-spin" />
       </div>
     </GKModalBackground>
